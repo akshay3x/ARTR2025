788a91193ca564aa3dc712e5da6547c97e55037c
akshay3x <chandawalear@gmail.com>
2025-09-15 17:48:29 +0530

    [ARC][Vulkan]01-Windows-25-Pipeline-Layout

---
+++ 01-Windows/03-Triangle/25-Pipeline-Layout/build.bat
@@ -0,0 +1,14 @@
+del vk.obj 
+del DebugLog.txt 
+del Application.exe
+
+del vk.res
+
+cl.exe /c /EHsc vk.c /I C:\VulkanSDK\Vulkan\Include
+
+rc vk.rc 
+
+link.exe vk.obj vk.res /LIBPATH:C:\VulkanSDK\Vulkan\Lib user32.lib gdi32.lib /SUBSYSTEM:WINDOWS /out:Application.exe
+
+Application.exe
+
